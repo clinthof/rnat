ebe4fb6cadc48ec453c94aabbafabc024a4d4bb1
clinthof <73394309+clinthof@users.noreply.github.com>
2023-03-19 19:25:39 -0400

    Add pre-commit and lint deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,44 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+    "react-native/react-native": true,
+    "jest/globals": true
+  },
+  extends: [
+    "plugin:react/recommended",
+    "prettier",
+    "eslint:recommended",
+    "plugin:jest/recommended"
+  ],
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  plugins: ["react", "react-native", "detox"],
+  ignorePatterns: ["!.*", "dist", "node_modules"],
+  rules: {
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"]
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "no-undef": "off"
+      }
+    }
+  ]
 };
